8ff209c314d6c53cf1f06c6547c65e70b1e4bf48
zachary-peasley <zpman95@hotmail.com>
2019-10-07 15:06:24 -0500

    Downloaded files, get request right

--- server.js
+++ server.js
@@ -1,9 +1,39 @@
 const express = require('express');
 
 const db = require('./data/dbConfig.js');
-
+const knex = require('knex')
 const server = express();
 
 server.use(express.json());
+
+
+server.get('/api/accounts/', (req, res) => {
+    db('accounts')
+    .select('name', 'budget')
+    .then(accounts => {
+        console.log(accounts);
+        res.status(200).json(accounts)
+    })
+    .catch(error => {
+        console.log(error);
+        res.json(error)
+    })
+})
+
+server.get('/api/accounts/:id', (req, res) => {
+    db('accounts')
+    .where({ id: req.params.id})
+        .first()
+        .then(response => {
+            res.status(200).json(response).first();
+        })
+        .catch(error => {
+            console.log(error).json(error)
+        })
+    })
+
+
+
+
 
 module.exports = server;
